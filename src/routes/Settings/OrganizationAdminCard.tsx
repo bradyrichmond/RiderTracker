a96945f4499c99e493ac4911e4b2f03a2f52c09d
Brady Richmond <brady@bradymrichmond.com>
2024-04-22 22:43:53 -0700

    fetching org admins and displaying admin cards

--- src/routes/Settings/OrganizationAdminCard.tsx
+++ src/routes/Settings/OrganizationAdminCard.tsx
@@ -2,26 +2,33 @@ import { AdminType } from "@/types/AdminType"
 import { Avatar, Box, Paper, Typography } from "@mui/material"
 import { useMemo } from "react"
 import PersonIcon from '@mui/icons-material/Person'
+import Grid from "@mui/material/Unstable_Grid2/Grid2"
 
 const OrganizationAdminCard = ({ id, firstName, lastName, title, email }: AdminType) => {
     const userFullName = `${firstName} ${lastName}`
     const profileUrl = useMemo(() => `https://s3.us-west-2.amazonaws.com/ridertracker.profileimages/${id}.jpg`, [id])
 
     return (
-        <Paper elevation={4}>
-            <Box sx={{ p: '2rem', display: 'flex', flexDirection: 'row' }}>
-                <Box sx={{ mr: '2rem' }}>
-                    <Avatar sx={{height: 200, width: 200}} src={profileUrl} alt={userFullName}>
-                        <PersonIcon fontSize='large' />
-                    </Avatar>
+        <Grid xs={12} sm={6}>
+            <Paper elevation={4}>
+                <Box sx={{ p: '2rem', display: 'flex', flexDirection: 'row' }}>
+                    <Box sx={{ mr: '2rem' }}>
+                        <Avatar sx={{ height: 150, width: 150, fontSize: '3rem' }} src={profileUrl} alt={userFullName}>
+                            <PersonIcon fontSize='inherit' />
+                        </Avatar>
+                    </Box>
+                    <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                        <Box>
+                            <Typography variant='h4'>{userFullName}</Typography>
+                            <Typography variant='subtitle1'>{title}</Typography>
+                        </Box>
+                        <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column', justifyContent: 'flex-end' }}>
+                            <Typography>Email: {email}</Typography>
+                        </Box>
+                    </Box>
                 </Box>
-                <Box sx={{ flex: 1 }}>
-                    <Typography variant='h4'>{userFullName}</Typography>
-                    <Typography variant='subtitle1'>{title}</Typography>
-                    <Typography>Email: {email}</Typography>
-                </Box>
-            </Box>
-        </Paper>
+            </Paper>
+        </Grid>
     )
 }
 
--- src/routes/Settings/OrganizationAdminSettings.tsx
+++ src/routes/Settings/OrganizationAdminSettings.tsx
@@ -1,15 +1,46 @@
+import { ApiContext } from "@/contexts/ApiContextProvider"
+import { RoleContext } from "@/contexts/RoleContextProvider"
+import { AdminType } from "@/types/AdminType"
 import { Card, Typography } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
+import { useContext, useEffect, useState } from "react"
+import OrganizationAdminCard from "./OrganizationAdminCard"
 
 const OrganizationAdminSettings = () => {
+    const { organizationId } = useContext(RoleContext)
+    const { api } = useContext(ApiContext)
+    const [ admins, setAdmins ] = useState<AdminType[]>([])
+
+    useEffect(() => {
+        getAdmins()
+    }, [])
+
+    const getAdmins = async () => {
+        const fetchedAdmins = await api.execute(api.organizations.getOrganizationAdmins, [organizationId])
+        setAdmins(fetchedAdmins)
+    }
+
     return (
-        <Grid sm={12} md={6}>
+        <Grid xs={12}>
             <Card sx={{ p: '2rem' }}>
                 <Typography variant='h4' sx={{ pb: '.5rem' }}>
                     Organization Admins
                 </Typography>
-                Need to figure out how to get users in group just for one org
-                BIG problem...maybe need to switch to generating cognito pools for each org
+                <Grid xs={12}>
+                    {admins.length > 0 ?
+                        admins.map((a) => <OrganizationAdminCard 
+                                key={a.id}
+                                id={a.id}
+                                organizationId={a.organizationId}
+                                firstName={a.firstName}
+                                lastName={a.lastName}
+                                email={a.email}
+                                title={a.title}
+                            />)
+                        :
+                        null
+                    }
+                </Grid>
             </Card>
         </Grid>
     )
