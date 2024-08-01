349cf69b3b61985a1ffd8d512499f8ec6d6ac035
Brady Richmond <richmond.brady@gmail.com>
2024-08-01 16:00:08 -0700

    fix generation

--- amplify/data/custom-authorizer/resource.ts
+++ amplify/data/custom-authorizer/resource.ts
@@ -1,10 +1,18 @@
 import { defineFunction } from '@aws-amplify/backend'
-import * as settings from '../../../amplify_outputs.json'
+import * as initialSettings from '../../../amplify_outputs.json'
+
+const settings: {
+  version: string
+  auth?: {
+    user_pool_id: string
+    user_pool_client_id: string
+  }
+} = initialSettings
 
 export const customAuthorizer = defineFunction({
   name: 'custom-authorizer',
   environment: {
-    USER_POOL_ID: settings.auth.user_pool_id,
-    WEB_CLIENT_ID: settings.auth.user_pool_client_id
+    USER_POOL_ID: settings.auth?.user_pool_id ?? '',
+    WEB_CLIENT_ID: settings.auth?.user_pool_client_id ?? ''
   }
 })
